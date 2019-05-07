47747e978a33e5458a1c2fc604ee944e89016f83
pabascom <phillip.bascom@mobileappscompany.us>
2019-05-06 20:54:23 -0400

    first commit

---
+++ app/src/main/java/phil/homework/week1day5take3_feliznavidrawer/MainActivity.kt
@@ -0,0 +1,12 @@
+package phil.homework.week1day5take3_feliznavidrawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
